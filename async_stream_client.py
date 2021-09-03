f3930fa13275586e73c03f01ff003d433b0e5e20
Honkerberg <ostadalmartin96@gmail.com>
2021-09-03 09:01:12 +0200

    Added try except if not connect

--- async_stream_client.py
+++ async_stream_client.py
@@ -1,12 +1,18 @@
 import socket
+import asyncio
+
 
 # Connection config
 HOST = '127.0.0.1'
 PORT = 20001
 
 with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    data = 'Help' + '\r' + '\n' # Vzdy pouzit \r a \n - CR LF (Carriage return + Line feed)
-    s.send(data.encode())
-    serverdata = s.recv(1024)
-    print(serverdata.decode())
+    try:
+        s.connect((HOST, PORT))
+        data = 'Help' + '\r' + '\n' # Vzdy pouzit \r a \n - CR LF (Carriage return + Line feed)
+        s.send(data.encode())
+        serverdata = s.recv(1024)
+        print(serverdata.decode())
+        s.close()
+    except:
+        print('Not connected! Make sure that server is running.')
